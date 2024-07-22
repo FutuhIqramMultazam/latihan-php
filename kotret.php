d3bf38f1d7da215f3361c716bb3c2ab3a4d41771
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-07-22 15:11:32 +0700

    menghapus sebagian img

--- kotret.php
+++ kotret.php
@@ -1,11 +1,33 @@
 <?php
-require 'fungsi.php';
-
 
 if (isset($_POST["buka"])) {
-    var_dump($_POST);
-    var_dump($_FILES);
+    var_dump(tes($_POST));
 }
+
+function tes($post)
+{
+    $post;
+    $gambar = gambar();
+    $ary = [$post, $gambar];
+
+    return $ary;
+}
+
+function gambar()
+{
+    $nama = $_FILES["gambar"]["name"];
+    $error = $_FILES["gambar"]["error"];
+    $tempat = $_FILES["gambar"]["tmp_name"];
+    $ukuran = $_FILES["gambar"]["size"];
+    $pecah = explode('.', $nama);
+    $ambil = strtolower(end($pecah));
+    $namai = uniqid();
+    $namai .= ".";
+    $namai .= $ambil;
+    $re = [$namai, $error, $tempat, $ukuran];
+    return $re;
+}
+
 ?>
 
 
@@ -18,30 +40,44 @@ if (isset($_POST["buka"])) {
     <title>Document</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="icons/bootstrap-icons.css">
 </head>
 
 <body>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="text" name="nama"><br>
 
-        <select name="jk" required>
+
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="w-25 mt-3">
+            <span class="input-group-text"><i class="bi bi-lock"></i></span>
+            <div class=" form-floating position-relative">
+                <input class="form-control" type="text" name="nama" placeholder=""><br>
+                <label>isi nama</label>
+            </div>
+        </div>
+
+        <select class="form-select w-25" name="jk" required>
             <option selected disabled>-- Pilih Jenis Kelamin --</option>
             <option value="pria">Pria</option>
             <option value="wanita">Wanita</option>
         </select><br>
-        <input type="number" name="lahir"><br>
-        <input type="file" name="gambar"><br>
-        <button name="buka">buka</button>
+
+        <div class="form-floating mb-3 w-25 ">
+            <input type="number" class="form-control" name="usia" id="usia" placeholder="" />
+            <label for="usia">Usia</label>
+        </div>
+
+        <input class="form-control w-25" type="file" name="gambar"><br>
+        <div class="d-grid gap-2 w-25">
+            <button type="submit" name="buka" id="buka" class="btn btn-success">
+                Button
+            </button>
+        </div>
+
     </form>
 
-    <div class=" form-floating mb-3 w-25">
-        <input type="password" class="form-control" name="sandi" placeholder="" />
-        <label class="form-label">Password</label>
-    </div>
-
-
-
-
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
+
 
 </html>
